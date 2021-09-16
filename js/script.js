b6a3ff8be864b184b7db47daf27fcbf6ce0baae1
Mavi Bucheli <mavibucheli@gmail.com>
2021-09-15 23:20:04 -0400

    got data to display! Now to iterate over ingredient and health label arrays.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,42 @@
-// let recipeData;
+
 const $dish = $("#dish");
 const $instructions = $("#instructions");
 const $ingredients = $("#ingredients");
 const $labels = $("#labels");
 const $calories = $("#calories");
+const $input = $('input[type="text"]');  
 
-$.ajax({
-    url: "https://api.edamam.com/api/recipes/v2?type=public&q=beef+broccoli+soy&app_id=1592f293&app_key=3badd2b21b26f16adc0d2e2d0b47e094&ingr=5-6"
-}).then(
-    (data) => {
-        console.log(data);
-        $dish.text(data.);// need to figure out how to access data from multiple arrays
-        // $instructions.text(data.);
-        // $ingredients.text(data.);
-        // $labels.text(data.);
-        // $calories.text(data.)
-    }
-);
+function handleGetData(event) {
+    event.preventDefault();
+
+    userInput = $input.val();
+
+    $.ajax({
+        url: "https://api.edamam.com/api/recipes/v2?type=public&q=" + userInput + "&app_id=1592f293&app_key=3badd2b21b26f16adc0d2e2d0b47e094&ingr=3-10"
+    }).then(
+        (data) => {
+            console.log(data);
+            $dish.text(data.hits[0].recipe.label);// these are only accessing one recipe, I need to access all of them in order to randomize recipes
+            $instructions.text(data.hits[0].recipe.url);
+            $ingredients.text(data.hits[0].recipe.ingredientLines[0]);
+            $labels.text(data.hits[0].recipe.healthLabels[0]);
+            $calories.text(data.hits[0].recipe.calories);
+        },
+        (error) => {
+            console.log("Error: ", error);
+        }
+    );
+}
+
+$('form').on('submit', handleGetData);
+
+
+
+
+
+    // steps I need to take! Finally was able to access the right info using dot notation
+// things I need to remind myself: 
+    // take baby steps
+    // ingredients and health labels needs to iterating through array of ingredients and labels
+    // instructions need to open up url and access the recipe 
+    // individual iterating functions for each ingr and label per recipe
