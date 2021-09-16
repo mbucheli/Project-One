184b4ad99f13b2cf5f70ac61d4a5bc86d6155402
Mavi Bucheli <mavibucheli@gmail.com>
2021-09-16 18:22:54 -0400

    Functionality is done! Had to rework a few things so my app wouldn't be clutterd.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
 const $dish = $("#dish");
+const $dishPhoto = $("#dishPhoto");
 const $instructions = $("#instructions");
 const $ingredients = $("#ingredients");
 const $labels = $("#labels");
@@ -15,11 +15,12 @@ function handleGetData(event) {
         url: "https://api.edamam.com/api/recipes/v2?type=public&q=" + userInput + "&app_id=1592f293&app_key=3badd2b21b26f16adc0d2e2d0b47e094&ingr=3-10"
     }).then(
         (data) => {
-            console.log(data);
+            // console.log(data);
             $dish.text(data.hits[0].recipe.label);// these are only accessing one recipe, I need to access all of them in order to randomize recipes
-            $instructions.text(data.hits[0].recipe.url);
-            $ingredients.text(data.hits[0].recipe.ingredientLines[0]);
-            $labels.text(data.hits[0].recipe.healthLabels[0]);
+            $dishPhoto.attr("src", data.hits[0].recipe.image);
+            $instructions.attr("href", data.hits[0].recipe.shareAs);
+            $ingredients.text(data.hits[0].recipe.ingredientLines);
+            $labels.text(data.hits[0].recipe.healthLabels);
             $calories.text(data.hits[0].recipe.calories);
         },
         (error) => {
@@ -29,14 +30,3 @@ function handleGetData(event) {
 }
 
 $('form').on('submit', handleGetData);
-
-
-
-
-
-    // steps I need to take! Finally was able to access the right info using dot notation
-// things I need to remind myself: 
-    // take baby steps
-    // ingredients and health labels needs to iterating through array of ingredients and labels
-    // instructions need to open up url and access the recipe 
-    // individual iterating functions for each ingr and label per recipe
